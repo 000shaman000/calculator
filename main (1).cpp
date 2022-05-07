7070d65d78afa6118c26bc77ca98112968fc4c5a
000shaman000 <100999810+000shaman000@users.noreply.github.com>
2022-05-08 01:05:19 +0800

    Add files via upload

---
+++ main (1).cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+
+using namespace std;
+int a,b;
+int main()
+{
+    cout<<"Hello World";
+    
+    std::cin >> a;
+    std::cin >> b;
+    
+if ( a > b && a < 10)
+cout<<123;
+else
+cout<<"dDL";
+}
